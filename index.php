5992a18804685721e7b14d494fc6eb850b8e73fe
MikZarZSG <mikzarzsg@gmail.com>
2020-10-14 20:11:06 +0200

    PobierzIloscDni - funkcja

--- index.php
+++ index.php
@@ -11,43 +11,39 @@
         <h1>Kalendarz</h1>
     
 <?php
+    
+    function pobierzIloscDni($miesiac, $rok) {
+        switch($miesiac) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 2:
+                if(($rok % 4 == 0 && $rok % 100 != 0) || $rok % 400 == 0) return 29;
+                return 28;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return 31;
+        }
+    }
+    
+        
+    
     //Pobranie danych dot. daty
     $dzien = date("d");
     $miesiac = date("m");
     $rok = date("Y");
-    $dni;
+    $dni = pobierzIloscDni($miesiac, $rok);
         
     echo "<h2>Miesiąc: $miesiac</h2>";
-    
-    //Ilość dni w zależności od miesiąca
-    switch($miesiac) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            $dni = 31;
-            break;
-        case 2:
-            if(($rok % 4 == 0 && $rok % 100 != 0) || $rok % 400 == 0) $dni = 29;
-            else $dni = 28;
-            break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            $dni = 30;
-            break;
-        default:
-            $dni = 31;
-    }
-    
-//    echo $dzien . "<br>";
-//    echo $miesiac . "<br>";
-//    echo $rok . "<br>";
-//    echo $dni . "<br>";
         
     for($i = 1; $i <= $dni; $i++) {
         if($i == $dzien) {
