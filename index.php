5fbb53c42f0b574e080af1839ae5371be15971eb
MikZarZSG <mikzarzsg@gmail.com>
2020-10-16 13:17:18 +0200

    Generowanie kalendarza w funkcji

--- index.php
+++ index.php
@@ -34,6 +34,55 @@
                 return 31;
         }
     }
+        
+    function wygenerujKalendarz($dzienDzisiaj, $miesiac, $rok, $iloscDni, $dzienTygodniaPierwszego) {
+        echo "<h2>$miesiac.$rok</h2>";
+        
+        echo '<div class="kalendarz">';
+
+        $iterTyg = 1;
+        $iterDzien = 0;
+
+        while($iterTyg <= 7 || $iterDzien <= $dni) {
+            //Przed miesiącem
+            if($iterDzien == 0) {
+                if($iterTyg < $dzienTygodniaPierwszego) {
+                    echo '<div class="dzien pusty"> </div>';
+                } else { $iterDzien++; }
+            }
+
+            if($iterDzien >= 1 && $iterDzien <= $iloscDni) {
+                //W trakcie miesiąca
+                if($iterDzien == $dzienDzisiaj) {
+                    echo '<div class="dzien dzisiaj">' . $iterDzien . '</div>';
+                } else {
+                    echo '<div class="dzien">' . $iterDzien . '</div>';
+                }
+                $iterDzien++;
+            } else if($iterDzien > $iloscDni){
+                //Po miesiącu
+                echo '<div class="dzien pusty"> </div>';
+            }
+
+            if($iterTyg < 7) {
+                //KOlejny dzień tygodnia
+                $iterTyg++;
+            }
+            else if($iterTyg == 7 && $iterDzien > $iloscDni) {
+                //Koniec generowania kalendarza
+                break;
+            } 
+            else {
+                //Nowa linia - nowy tydzień
+                echo '<div class="clear"></div>';
+                $iterTyg = 1;
+            }
+        }
+
+        echo '<div class="clear"></div>';
+
+        echo '</div>';
+    }
     
         
     
@@ -44,49 +93,9 @@
     $dni = pobierzIloscDni($miesiac, $rok);
     $pierwszyDzien = mktime(1, 0, 0, $miesiac, 1, $rok);
     $dzienTygodnia = date("N", $pierwszyDzien);
-        
-    echo "<h2>$miesiac.$rok</h2>";
-        
-    echo '<div class="kalendarz">';
     
-    $iterTyg = 1;
-    $iterDzien = 0;
-        
-    while($iterTyg <= 7 || $iterDzien <= $dni) {
-        //Przed miesiącem
-        if($iterDzien == 0) {
-            if($iterTyg < $dzienTygodnia) {
-                echo '<div class="dzien pusty"> </div>';
-            } else { $iterDzien++; }
-        }
-        
-        if($iterDzien >= 1 && $iterDzien <= $dni) {
-            //W trakcie miesiąca
-            if($iterDzien == $dzien) {
-                echo '<div class="dzien dzisiaj">' . $iterDzien . '</div>';
-            } else {
-                echo '<div class="dzien">' . $iterDzien . '</div>';
-            }
-            $iterDzien++;
-        } else if($iterDzien > $dni){
-            //Po miesiącu
-            echo '<div class="dzien pusty"> </div>';
-        }
-            
-        if($iterTyg < 7) {
-            $iterTyg++;
-        }
-        else if($iterTyg == 7 && $iterDzien > $dni){
-            break;
-        } else if($iterTyg == 7) {
-            echo '<div class="clear"></div>';
-            $iterTyg = 1;
-        }
-    }
-    
-    echo '<div class="clear"></div>';
-        
-    echo '</div>';
+    //Generowanie kalendarza
+    wygenerujKalendarz($dzien, $miesiac, $rok, $dni, $dzienTygodnia);
 ?>
    
     </div>
